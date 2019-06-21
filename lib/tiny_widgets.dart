a5b01b3ffb2c91ae51c67d9a2c52d1a6e9aecc64
Saif Allah Khaled <Saifallak@yahoo.com>
2019-06-21 07:31:03 +0200

    export

--- lib/tiny_widgets.dart
+++ lib/tiny_widgets.dart
@@ -1,3 +1,5 @@
 library tiny_widgets;
 
 export 'package:tiny_widgets/src/tiny_container.dart';
+export 'package:tiny_widgets/src/tiny_rating.dart';
+export 'package:tiny_widgets/src/tiny_match_card.dart';
