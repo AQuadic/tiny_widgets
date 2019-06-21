3470b72f0b4d634961f52f6057e4f9a1c5f02a80
Saif Allah Khaled <Saifallak@yahoo.com>
2019-06-21 07:36:27 +0200

    export

--- lib/src/tiny_match_card.dart
+++ lib/src/tiny_match_card.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../tiny_widgets.dart';
+import 'tmc_src/team_flag.dart';
+import 'tmc_src/verses_container.dart';
+
 class TinyMatchCard extends StatelessWidget {
   final String firstTeam;
 
@@ -33,6 +37,44 @@ class TinyMatchCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget widget;
+
+    widget = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TinyContainer(
+          innerPadding: EdgeInsets.all(8),
+          width: MediaQuery.of(context).size.width * 0.9,
+          maxWidth: MediaQuery.of(context).size.width * 0.9,
+          maxHeight: MediaQuery.of(context).size.width * 0.3,
+          backgroundColor: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[400],
+              blurRadius: 5,
+              spreadRadius: 2,
+            )
+          ],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              TeamFlag(
+                text: firstTeam,
+                flagWidget: firstTeamFlag,
+              ),
+              VersesContainer(
+                timeText: timeText,
+                dateText: dateText,
+                groupText: groupText,
+              ),
+              TeamFlag(
+                text: secondTeam,
+                flagWidget: secondTeamFlag,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
 
     if (onTap != null) {
       widget = InkWell(
