a9a0026b9ac9d3a399e7abd8a8ac55e27f1c0bad
Eslam El-feky <eslamira@users.noreply.github.com>
2019-07-18 06:23:16 +0200

    validators

---
+++ lib/src/validators.dart
@@ -0,0 +1,59 @@
+class TinyValidators {
+  /// making singleton
+  static TinyValidators _instance = TinyValidators.internal();
+  TinyValidators.internal();
+  factory TinyValidators() => _instance;
+
+  /// Validating email address
+  String emailValidator(String value) {
+    if (value.length < 1)
+      return 'Can\'t be empty';
+    else {
+      if (value.length > 3 && value.contains('@') && value.contains('.')) {
+        return null;
+      } else {
+        return 'Wrong Email Address';
+      }
+    }
+  }
+
+  /// Validating password
+  String passValidator(String value) {
+    if (value.length < 1)
+      return 'Can\'t be empty';
+    else {
+      if (value.length > 6 &&
+          value.contains(RegExp('[A-Z]')) &&
+          value.contains(RegExp('[a-z]')) &&
+          value.contains(RegExp('[1-9]'))) {
+        return null;
+      } else {
+        return 'Please Enter Strong Pass with Letters and Numbers';
+      }
+    }
+  }
+
+  /// Validating name (just can't be empty for now)
+  String nameValidator(String value) {
+    if (value.length < 1)
+      return 'Can\'t be empty';
+    else
+      return null;
+  }
+
+  /// Validating user's phone number
+  String phoneValidator(String value) {
+    if (value.length < 1)
+      return 'Can\'t be empty';
+    else if (value.length == 11 &&
+        (value.substring(0, 2) == '010' ||
+            value.substring(0, 2) == '011' ||
+            value.substring(0, 2) == '012' ||
+            value.substring(0, 2) == '015') &&
+        !value.contains(RegExp('[a-z]')) &&
+        !value.contains(RegExp('[A-Z]')))
+      return null;
+    else
+      return 'Wrong Phone Number';
+  }
+}
