b740a9a8d9e3307bc8fdf6abfb4103b7857b24eb
Eslam El-feky <eslamira@users.noreply.github.com>
2019-07-18 06:24:42 +0200

    export validators

--- lib/tiny_widgets.dart
+++ lib/tiny_widgets.dart
@@ -3,3 +3,4 @@ library tiny_widgets;
 export 'package:tiny_widgets/src/tiny_container.dart';
 export 'package:tiny_widgets/src/tiny_match_card.dart';
 export 'package:tiny_widgets/src/tiny_rating.dart';
+export 'package:tiny_widgets/src/validators.dart';
